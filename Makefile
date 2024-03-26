0083892c6b1e75d0a304a3010e90679d045c3db6
Carlos Henrique Lopes Zansavio <carloshlzansavio@gmail.com>
2024-03-26 10:08:20 -0300

    Add mock library

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
-test:
+pytest:
 	python -m pytest tests/services/batch_insert_people_service_tests.py
+
+unittest:
+	python -m unittest tests/services/unittest_batch_insert_people_service.py
--- tests/services/unittest_batch_insert_people_service.py
+++ tests/services/unittest_batch_insert_people_service.py
@@ -1,26 +1,32 @@
 import unittest
-from injector import Injector, Module
+from unittest.mock import Mock
+from injector import Injector
 from interfaces.api_service import ApiService
 from interfaces.storage_service import StorageService
 from services.batch_insert_people_service import BatchInsertPeopleService
-from tests.mocks.mock_api_service import MockApiService
-from tests.mocks.mock_storage_service import MockStorageService
-
-
-class BatchInsertPeopleModule(Module):
-    def configure(self, binder):
-        binder.bind(StorageService, MockStorageService)
-        binder.bind(ApiService, MockApiService)
 
 
 class TestBatchInsertPeople(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super(TestBatchInsertPeople, self).__init__(*args, **kwargs)
+        self.api_service_mock = Mock()
+
+    def _configure(self, binder):
+        binder.bind(StorageService, Mock())
+        binder.bind(ApiService, self.api_service_mock)
+
     def setUp(self):
-        self.__injector = Injector(BatchInsertPeopleModule())
+        self.__injector = Injector(self._configure)
         self.__service = self.__injector.get(BatchInsertPeopleService)
 
     def test_batch_insert_people(self):
+        # Arrange
+        self.api_service_mock.batch_insert_people.return_value = "hello world"
+
+        # Act
         response = self.__service(folder='test', mailing_file='file')
 
+        # Assert
         self.assertEqual(response, "hello world")
 
 
